b5862da6bb8dac97bf87693b85dee7a93431ccb4
Ruslan Muzhichkov <chaika1201@gmail.com>
2024-08-09 01:18:26 +0300

    Create new branch for hometask4

--- main.cpp
+++ main.cpp
@@ -1,131 +1,66 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include <map>
+#include <list>
+#include <tuple>
 
-template <typename T, size_t size>
-struct my_allocator {
-    typedef T value_type;
-    typedef T* pointer;
-    typedef const T* const_pointer;
-    typedef T& reference;
-    typedef const T& const_reference;
+template<typename ...>
+using void_t=void;
 
-    my_allocator () noexcept : memory{std::malloc(sizeof(T) * size)}, pos{0} {}
-    ~my_allocator() { std::free(memory); }
-
-    template <class U> my_allocator  (const my_allocator <U, size>&) noexcept {}
-
-    T* allocate (std::size_t n) {
-        if (pos + n > size)
-            throw std::bad_alloc();
-        int cur = pos;
-        pos += n;
-        return static_cast<T*>(memory) + cur;
-    }
-
-    void deallocate (T* p, std::size_t n) {}
-
-    template <class Up, class... Args>
-    void construct(Up* p, Args&&... args) {
-        ::new ((void*)p) Up(std::forward<Args>(args)...);
-    }
-
-    void destroy(pointer p) {
-        p->~T();
-    }
-
-    template<typename U>
-    struct rebind
-    {
-        typedef my_allocator<U,size> other;
-    };
-
-private:
-    void* memory;
-    size_t pos;
+template<typename T, typename U = void>
+struct is_container : std::false_type {
 };
 
-template <typename T, typename Allocator = std::allocator<T>>
-struct my_container {
-    my_container() = default;
-    ~my_container() = default;
-    void push_back(const T& x) {
-        if (size == capacity) {
-            capacity = capacity * 2 + 1;
-            T* newData = alloc.allocate(capacity);
-            if (data != nullptr)
-                std::copy(data, data + size, newData);
-            std::swap(newData, data);
-            alloc.deallocate(newData, size);
-        }
-        alloc.construct(&data[size], x);
-        ++size;
-    }
+template<typename T>
+struct is_container<T, void_t<typename T::iterator>> : std::true_type {
 
-    T* begin() {
-        return data;
-    }
-
-    T* end() {
-        return &data[size];
-    }
-
-private:
-    std::size_t size = 0;
-    std::size_t capacity = 0;
-    T* data = nullptr;
-    Allocator alloc;
 };
 
-int factorial(int n, int* ar) {
-    if (n >= 1) {
-        ar[n] = ar[n-1] * n;
-    }
-    return ar[n];
+template <typename T>
+struct is_tuple : std::false_type {
+
+};
+
+template <typename ...Ts>
+struct is_tuple<std::tuple<Ts...>> : std::true_type{
+
+};
+
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value>::type
+print_ip(const T& val) {
+
 }
 
-int main(int argc, char const *argv[])
-{
-    int fac[10]{1};
+template <typename T>
+typename std::enable_if<std::is_polymorphic<T>::value>::type
+print_ip(const T& val) {
 
-    // std::map std::allocator
-    std::map<int, int> m1;
-    for (int i = 0; i < 10; ++i) {
-        m1[i] = factorial(i, fac);
-    }
+}
 
-    // std::map my_allocator
-    auto m2 = std::map<
-        int,
-        int,
-        std::less<int>,
-        my_allocator<
-            std::pair<
-                const int, int>, 10>>{};
+template <typename T>
+typename std::enable_if<is_container<T>::value>::type
+print_ip(const T& val) {
 
-    for (int i = 0; i < 10; ++i) {
-        m2[i] = factorial(i, fac);
-    }
-
-    for (auto &p : m2) {
-        std::cout << p.first << " " << p.second << "\n";
-    }
-
-    // my_container std::allocator
-    my_container<int> c1;
-    for (int i = 0; i < 10; ++i) {
-        c1.push_back(i);
-    }
+}
 
 
-    // my_container my_allocator
-    my_container<int, my_allocator<int, 30>> c2;
-    for (int i = 0; i < 10; ++i) {
-        c2.push_back(i);
-    }
+template <typename T>
+typename std::enable_if<is_tuple<T>::value>::type
+print_ip(const T& val) {
 
-    for (auto &e : c2) {
-        std::cout << e << "\n";
-    }
+}
+
+
+int main() {   
+    print_ip ( int8_t{-1} ); // 255
+    print_ip ( int16_t{0} ); // 0.0
+    print_ip ( int32_t{2130706433} ); // 127.0.0.1
+    print_ip ( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41
+    print_ip ( std::string{"Hello, World!"} ); // Hello, World!
+    print_ip ( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400
+    print_ip ( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100
+    print_ip ( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
+
     return 0;
 }
--- utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-
-namespace Utils {
-    std::vector<std::string> split(const std::string &str, char d);
-}
